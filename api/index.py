74f2fb9cee7760d2f4b0f1fcd883b994663493fc
Julian <contactosjulit@gmail.com>
2024-06-02 13:20:18 -0300

    fix

--- api/index.py
+++ api/index.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template
 from tareas import asignar_tareas, personas, espacios, dias
 
 app = Flask(__name__)
-app.template_folder = "templates"
-app.static_folder = "static"
-
+app.template_folder = "C://Users//Usuario//source//cutl//templates"
+app.static_folder = "C://Users//Usuario//source//cutl//static"
 
 @app.route('/')
 def index():
+    print(app.template_folder)
+    print(app.static_folder)
     try:
         calendario = asignar_tareas(personas, espacios, dias)
         contador_tareas = {persona.nombre: len(persona.tareas_asignadas) for persona in personas}
