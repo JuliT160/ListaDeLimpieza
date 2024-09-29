a5ecbb606f37ff09c39e22cf3b9b260be00e7c56
Julian <contactosjulit@gmail.com>
2024-09-28 23:44:26 -0300

    configure env variables

--- api/firebase_config.py
+++ api/firebase_config.py
@@ -1,7 +1,21 @@
 import firebase_admin
+import os
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("api/lista-de-limpieza-353d33339f47.json")
+#cred = credentials.Certificate("api/lista-de-limpieza-353d33339f47.json")
+cred = credentials.Certificate({
+    "type": os.environ.get("TYPE"),
+    "project_id": os.environ.get("PROJECT_ID"),
+    "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("PRIVATE_KEY"),
+    "client_email": os.environ.get("CLIENT_EMAIL"),
+    "client_id": os.environ.get("CLIENT_ID"),
+    "auth_uri": os.environ.get("AUTH_URI"),
+    "token_uri": os.environ.get("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("CLIENT_X509_CERT_URL"),
+})
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
