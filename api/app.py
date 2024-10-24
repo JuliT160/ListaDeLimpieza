c6abeb4a8426cffb7c9a7c5b431dca997ae929ee
Julian <contactosjulit@gmail.com>
2024-10-24 10:12:04 -0300

    deployRender

--- api/app.py
+++ api/app.py
@@ -256,4 +256,4 @@ def generar_calendario():
     contador_tareas = contar_tareas_mejorado(calendario)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
